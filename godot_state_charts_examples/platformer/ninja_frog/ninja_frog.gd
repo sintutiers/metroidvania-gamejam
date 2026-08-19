2495baf6ec14223b8caa3c09e2b6f21166eb54d3
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-08-19 14:02:05 +0200

    make game runable + config

--- godot_state_charts_examples/platformer/ninja_frog/ninja_frog.gd
+++ godot_state_charts_examples/platformer/ninja_frog/ninja_frog.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-## Emitted when this node is clicked with a mouse 
-signal clicked(node:Node2D)
+## Emitted when this node is clicked with a mouse
+signal clicked(node: Node2D)
 
 const SPEED := 300.0
 const JUMP_VELOCITY := -400.0
@@ -9,17 +9,19 @@ const JUMP_VELOCITY := -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+## Flag indicating if the character was on the floor in the last frame.
+var _was_on_floor: bool = false
+
 @onready var _sprite: Sprite2D = $Sprite
 @onready var _state_chart: StateChart = $StateChart
 @onready var _animation_tree: AnimationTree = $AnimationTree
-@onready var _animation_state_machine: AnimationNodeStateMachinePlayback = _animation_tree.get("parameters/playback")
+@onready var _animation_state_machine: AnimationNodeStateMachinePlayback = _animation_tree.get(
+	"parameters/playback"
+)
 
-## Flag indicating if the character was on the floor in the last frame.
-var _was_on_floor:bool = false
 
 # In all states, move and slide and handle left/right movement and gravity.
-func _physics_process(delta:float) -> void:
-
+func _physics_process(delta: float) -> void:
 	# handle left/right movement
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
@@ -34,7 +36,7 @@ func _physics_process(delta:float) -> void:
 
 	# gravity handled in Grounded and Airborne states
 	move_and_slide()
-	
+
 	# if we are on the floor right now
 	if is_on_floor():
 		velocity.y = 0
@@ -48,8 +50,7 @@ func _physics_process(delta:float) -> void:
 		if _was_on_floor:
 			_was_on_floor = false
 			_state_chart.send_event("airborne")
-		
-	
+
 	# let the state machine know if we are moving or not
 	if velocity.length_squared() <= 0.005:
 		_animation_state_machine.travel("Idle")
@@ -61,7 +62,7 @@ func _physics_process(delta:float) -> void:
 
 
 ## Called in states that allow jumping, we process jumps only in these.
-func _on_jump_enabled_state_physics_processing(_delta:float) -> void:
+func _on_jump_enabled_state_physics_processing(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 		_state_chart.send_event("jump")
@@ -73,8 +74,10 @@ func _on_double_jump_jump() -> void:
 	_animation_state_machine.travel("DoubleJump")
 
 
-func _on_input_event(_viewport:Node, event:InputEvent, _shape_idx:int) -> void:
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	# if the left mouse button is up emit the clicked signal
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed() == false:
-			clicked.emit(self)
-
+	if (
+		event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT
+		and event.is_pressed() == false
+	):
+		clicked.emit(self)
--- godot_state_charts_examples/save_and_load/save_and_load.gd
+++ godot_state_charts_examples/save_and_load/save_and_load.gd
@@ -5,9 +5,9 @@ extends Node2D
 ## any other game specific state we want to save.
 const SAVE_PATH: StringName = "user://save_resource.tres"
 
-@onready var _state_chart: StateChart = %StateChart
-
 var _boxes: Array[ColorBox]
+
+@onready var _state_chart: StateChart = %StateChart
 
 
 func _ready() -> void:
@@ -15,7 +15,10 @@ func _ready() -> void:
 	_boxes.assign(get_tree().get_nodes_in_group("colorbox"))
 	# send an event whenever a box requests a color change
 	for box in _boxes:
-		box.color_change_requested.connect(func(): _state_chart.send_event(box.box_id + "-click"))
+		box.color_change_requested.connect(
+			func():
+				_state_chart.send_event(box.box_id + "-click"),
+		)
 
 
 ## Called when the "Save" button is pressed.
@@ -27,6 +30,7 @@ func save_state() -> void:
 
 	ResourceSaver.save(save_resource, SAVE_PATH)
 
+
 ## Called when the "Load" button is pressed.
 func load_state() -> void:
 	var save_resource: SaveResource = ResourceLoader.load(SAVE_PATH)
@@ -34,7 +38,7 @@ func load_state() -> void:
 	StateChartSerializer.deserialize(save_resource.state_chart, _state_chart)
 
 	# make a lookup table for the boxes
-	var box_lut: Dictionary = {}
+	var box_lut: Dictionary = { }
 	for node in _boxes:
 		box_lut[node.box_id] = node
 
@@ -42,4 +46,3 @@ func load_state() -> void:
 	for box_resource in save_resource.boxes:
 		var color_box: ColorBox = box_lut[box_resource.box_id]
 		color_box.load_state(box_resource)
-
